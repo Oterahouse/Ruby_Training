c6a9b7522b3e7b3e7cb52dfae7fb263c2dd278dd
おてら <65020071+Oterahouse@users.noreply.github.com>
2020-06-14 23:35:57 +0900

    Ruby Study using progate

--- training.rb
+++ training.rb
@@ -1,21 +1,29 @@
-class Menu
-  attr_accessor :name
-  attr_accessor :price
+# food.rbとdrink.rbを読み込むように書き換えてください
+require "./menu"
 
-  def initialize(name:, price:)
-    self.name = name
-    self.price = price
-  end
-  
-  def info
-    return "#{self.name} #{self.price}円"
-  end
-  
-  def get_total_price(count)
-    total_price = self.price * count
-    if count >= 3
-      total_price -= 100
-    end
-    return total_price
-  end
+# food1とfood2をFoodクラスのインスタンスに書き換えてください
+food1 = Menu.new(name: "ピザ", price: 800)
+food2 = Menu.new(name: "すし", price: 1000)
+# drink1とdrink2をDrinkクラスのインスタンスに書き換えてください
+drink1 = Menu.new(name: "コーラ", price: 300)
+drink2 = Menu.new(name: "お茶", price: 200)
+
+menus = [food1, food2, drink1, drink2]
+
+index = 0
+menus.each do |menu|
+  puts "#{index}. #{menu.info}"
+  index += 1
 end
+
+puts "--------------"
+puts "メニューの番号を選択してください"
+order = gets.chomp.to_i
+
+selected_menu = menus[order]
+puts "選択されたメニュー: #{selected_menu.name}"
+
+puts "個数を入力してください(3つ以上で100円割引)"
+count = gets.chomp.to_i
+
+puts "お会計は#{selected_menu.get_total_price(count)}円です"
