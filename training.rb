66409c8b3a6035f087e97fcc1ae8ef3babe4f425
おてら <65020071+Oterahouse@users.noreply.github.com>
2020-06-19 23:58:47 +0900

    Ruby on Rails study using progate

--- training.rb
+++ training.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user
+  
   def index
     @posts = Post.all.order(created_at: :desc)
   end
@@ -8,12 +10,21 @@ class PostsController < ApplicationController
   end
   
   def new
+    @post = Post.new
   end
   
   def create
-    @post = Post.new(content: params[:content])
-    @post.save
-    redirect_to("/posts/index")
+    @post = Post.new(
+      content: params[:content],
+      # user_idの値をログインしているユーザーのidにしてください
+      user_id: @current_user.params[:user_id]
+    )
+    if @post.save
+      flash[:notice] = "投稿を作成しました"
+      redirect_to("/posts/index")
+    else
+      render("posts/new")
+    end
   end
   
   def edit
@@ -23,15 +34,18 @@ class PostsController < ApplicationController
   def update
     @post = Post.find_by(id: params[:id])
     @post.content = params[:content]
-    @post.save
-    redirect_to("/posts/index")
+    if @post.save
+      flash[:notice] = "投稿を編集しました"
+      redirect_to("/posts/index")
+    else
+      render("posts/edit")
+    end
   end
   
   def destroy
-    # destroyアクションの中身を作成してください
     @post = Post.find_by(id: params[:id])
     @post.destroy
-    @post.save
+    flash[:notice] = "投稿を削除しました"
     redirect_to("/posts/index")
   end
   
