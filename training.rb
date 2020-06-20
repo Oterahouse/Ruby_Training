d2bc5b998eab2358f999a11b3167230a4865cf97
おてら <65020071+Oterahouse@users.noreply.github.com>
2020-06-20 23:50:37 +0900

    Ruby on Rails study using progate

--- training.rb
+++ training.rb
@@ -1,6 +1,4 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user
-  
   def index
     @posts = Post.all.order(created_at: :desc)
   end
@@ -10,20 +8,15 @@ class PostsController < ApplicationController
   end
   
   def new
-    @post = Post.new
   end
   
   def create
-    @post = Post.new(
-      content: params[:content],
-      # user_idの値をログインしているユーザーのidにしてください
-      user_id: @current_user.params[:user_id]
-    )
+    @post = Post.new(content: params[:content])
+    # 保存に成功した場合は投稿一覧ページ、保存に失敗した場合は新規投稿ページが表示されるようにif-else文を追加してください
     if @post.save
-      flash[:notice] = "投稿を作成しました"
       redirect_to("/posts/index")
     else
-      render("posts/new")
+      render("/posts/edit")
     end
   end
   
@@ -45,7 +38,6 @@ class PostsController < ApplicationController
   def destroy
     @post = Post.find_by(id: params[:id])
     @post.destroy
-    flash[:notice] = "投稿を削除しました"
     redirect_to("/posts/index")
   end
   
