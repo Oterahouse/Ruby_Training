2107571ccc265a00a65260f11a03149b898d09f0
おてら <65020071+Oterahouse@users.noreply.github.com>
2020-06-21 23:50:23 +0900

    Ruby on Rails study using progate lesson 5 end

--- training.rb
+++ training.rb
@@ -8,15 +8,17 @@ class PostsController < ApplicationController
   end
   
   def new
+    @post = Post.new
   end
   
   def create
     @post = Post.new(content: params[:content])
-    # 保存に成功した場合は投稿一覧ページ、保存に失敗した場合は新規投稿ページが表示されるようにif-else文を追加してください
     if @post.save
+      # 変数flash[:notice]に、指定されたメッセージを代入してください
+      flash[:notice] = "投稿を作成しました"
       redirect_to("/posts/index")
     else
-      render("/posts/edit")
+      render("posts/new")
     end
   end
   
@@ -38,6 +40,8 @@ class PostsController < ApplicationController
   def destroy
     @post = Post.find_by(id: params[:id])
     @post.destroy
+    # 変数flash[:notice]に、指定されたメッセージを代入してください
+    flash[:notice] = "投稿を削除しました"
     redirect_to("/posts/index")
   end
   
