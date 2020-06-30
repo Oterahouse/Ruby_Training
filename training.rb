1fefce0fe853badf4af08759da51b801425434e7
おてら <65020071+Oterahouse@users.noreply.github.com>
2020-06-30 23:58:11 +0900

    Ruby on Rails is studying using progate lesson 9

--- training.rb
+++ training.rb
@@ -1,89 +1,54 @@
-class UsersController < ApplicationController
-  before_action :authenticate_user, {only: [:index, :show, :edit, :update]}
-  before_action :forbid_login_user, {only: [:new, :create, :login_form, :login]}
-  # before_actionにensure_correct_userメソッドを指定してください
-  before_action :ensure_correct_user, {only: [:edit, :update]}
+class PostsController < ApplicationController
+  before_action :authenticate_user
   
   def index
-    @users = User.all
+    @posts = Post.all.order(created_at: :desc)
   end
   
   def show
-    @user = User.find_by(id: params[:id])
+    @post = Post.find_by(id: params[:id])
+    # 変数@userを定義してください
+    @user = User.find_by(id: @post.user_id)
+    
   end
   
   def new
-    @user = User.new
+    @post = Post.new
   end
   
   def create
-    @user = User.new(
-      name: params[:name],
-      email: params[:email],
-      image_name: "default_user.jpg",
-      password: params[:password]
+    @post = Post.new(
+      content: params[:content],
+      user_id: @current_user.id
     )
-    if @user.save
-      session[:user_id] = @user.id
-      flash[:notice] = "ユーザー登録が完了しました"
-      redirect_to("/users/#{@user.id}")
+    if @post.save
+      flash[:notice] = "投稿を作成しました"
+      redirect_to("/posts/index")
     else
-      render("users/new")
+      render("posts/new")
     end
   end
   
   def edit
-    @user = User.find_by(id: params[:id])
+    @post = Post.find_by(id: params[:id])
   end
   
   def update
-    @user = User.find_by(id: params[:id])
-    @user.name = params[:name]
-    @user.email = params[:email]
-    
-    if params[:image]
-      @user.image_name = "#{@user.id}.jpg"
-      image = params[:image]
-      File.binwrite("public/user_images/#{@user.image_name}", image.read)
-    end
-    
-    if @user.save
-      flash[:notice] = "ユーザー情報を編集しました"
-      redirect_to("/users/#{@user.id}")
-    else
-      render("users/edit")
-    end
-  end
-  
-  def login_form
-  end
-  
-  def login
-    @user = User.find_by(email: params[:email], password: params[:password])
-    if @user
-      session[:user_id] = @user.id
-      flash[:notice] = "ログインしました"
+    @post = Post.find_by(id: params[:id])
+    @post.content = params[:content]
+    if @post.save
+      flash[:notice] = "投稿を編集しました"
       redirect_to("/posts/index")
     else
-      @error_message = "メールアドレスまたはパスワードが間違っています"
-      @email = params[:email]
-      @password = params[:password]
-      render("users/login_form")
+      render("posts/edit")
     end
   end
   
-  def logout
-    session[:user_id] = nil
-    flash[:notice] = "ログアウトしました"
-    redirect_to("/login")
-  end
-  
-  # ensure_correct_userを定義してください
-  def ensure_correct_user
-    if @current_user.id != params[:id].to_i
-      flash[:notice] = "権限がありません"
-      redirect_to("/posts/index")
-    end
+  def destroy
+    @post = Post.find_by(id: params[:id])
+    @post.destroy
+    flash[:notice] = "投稿を削除しました"
+    redirect_to("/posts/index")
   end
   
 end
